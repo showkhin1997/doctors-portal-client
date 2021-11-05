987a21ef41d98d83cee36019b95a13c808d7a13c
showkhin1997 <showkhin15@gmail.com>
2021-11-05 11:49:46 +0600

    login ui

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Login from './Pages/Login/Login/Login';
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
           </Route>
           <Route path="/appointment">
             <Appointment></Appointment>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
           </Route>
         </Switch>
       </Router>
---
+++ src/Pages/Login/Login/Login.js
@@ -0,0 +1,52 @@
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import login from '../../../images/login.png';
+
+const Login = () => {
+    const [loginData, setLoginData] = useState({});
+
+    const handleOnChnage = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+
+    const handleSubmit = e => {
+        alert('hello')
+        e.preventDefault();
+    }
+    return (
+        <Container>
+            <Grid container spacing={2}>
+                <Grid sx={{ mt: 8 }} item xs={12} md={6}>
+                    <Typography variant="body1" gutterBottom>Login</Typography>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Email"
+                            name="email"
+                            onChange={handleOnChnage}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Password"
+                            name="password"
+                            onChange={handleOnChnage}
+                            type="password"
+                            variant="standard" />
+                        <Button sx={{ width: '75%', m: 1 }} variant="contained" type="submit">Login</Button>
+                    </form>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{ width: '100%' }} src={login} alt="" />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Login;
